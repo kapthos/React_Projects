33bbac2f68cbafd167d55094cf6945e424557afa
Juan Carlos Torterolo Pedroza <juan.torterolo@gmail.com>
2022-08-07 02:50:40 -0300

    First Commit
    Inciando calculadora

---
+++ calculator-react/src/App.js
@@ -0,0 +1,11 @@
+import Inicial from "./Components/Inicial";
+
+function App() {
+  return (
+    <div className="App">
+      <Inicial/>
+    </div>
+  );
+}
+
+export default App;
---
+++ calculator-react/src/Components/Inicial.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {MainDiv} from '../styled'
+
+    function Inicial() {
+        return (
+            <MainDiv>
+                Inicial
+            </MainDiv>
+        )
+    }
+
+export default Inicial
