020db1952dca5a70a31510576c97ff800c86431c
Juan Carlos Torterolo Pedroza <juan.torterolo@gmail.com>
2022-08-08 01:30:31 -0300

    calculadora funcionando sem decimal
    sem decimal

--- calculator-react/src/Components/Inicial.jsx
+++ calculator-react/src/Components/Inicial.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
-import {MainDiv} from '../styled'
+import {MainDiv, BotoesDiv, Visor} from '../styled'
 
-    function Inicial() {
+    function Inicial({handleValores, handleDisplay, handleReset, handleOperacoes, handleResult}) {
         return (
             <MainDiv>
-                Inicial
+                <Visor>
+                    <p>{handleDisplay()}</p>
+                </Visor>
+                <BotoesDiv>
+                    <button id='c'onClick={handleReset}>C</button>
+                    <button>%</button>
+                    <button onClick={() => handleOperacoes("/")}>/</button>
+                    <button onClick={() => handleValores("1")}>1</button>
+                    <button onClick={() => handleValores("2")}>2</button>
+                    <button onClick={() => handleValores("3")}>3</button>
+                    <button onClick={() => handleOperacoes("x")}>x</button>
+                    <button onClick={() => handleValores("4")}>4</button>
+                    <button onClick={() => handleValores("5")}>5</button>
+                    <button onClick={() => handleValores("6")}>6</button>
+                    <button onClick={() => handleOperacoes("-")}>-</button>
+                    <button onClick={() => handleValores("7")}>7</button>
+                    <button onClick={() => handleValores("8")}>8</button>
+                    <button onClick={() => handleValores("9")}>9</button>
+                    <button onClick={() => handleOperacoes("+")}>+</button>
+                    <button onClick={() => handleValores("0")}>0</button>
+                    <button onClick={() => handleValores(".")}>.</button>
+                    <button>DEL</button>
+                    <button onClick={() => handleResult()}>=</button>
+                </BotoesDiv>
             </MainDiv>
         )
     }
--- calculator-react/src/styled.js
+++ calculator-react/src/styled.js
@@ -1,5 +1,37 @@
 import styled from 'styled-components'
 
 export const MainDiv = styled.div`
+    background-color: #444;
     display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    margin: 100px auto;
+    padding: 8px;
+    width: 250px;
+    height: 300px;
+    border-radius: 10px;
+`
+
+export const Visor = styled.div`
+    display: flex;
+    width: 100%;
+    height: 50px;
+    background-color: green;
+
+    p{
+        margin-left: 4px;
+    }
+`
+
+export const BotoesDiv = styled.div`
+    display: grid;
+    column-gap: 4px;
+    row-gap: 4px;
+    height: 80%;
+    margin-top: 8px;
+    grid-template-columns: repeat(4, 1fr);
+
+    #c {
+        grid-column: 1 / 3;
+    }
 `
